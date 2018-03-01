e68d332aa3819a34896ca501573f93cc6a99be71
aocalderon <andress.calderon@gmail.com>
2018-03-01 08:55:08 -0800

    Adding first set of experiments for FlockFinder SpatialJoin and MergeLast variants...

---
+++ Experiments/FlockFinder_v1.0/ExecutionTime.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+JAR_FILE="/home/acald013/Research/PFlock/target/scala-2.11/pflock_2.11-2.0.jar"
+PARTITIONS=28
+CORES=28
+DATASET_PATH="Datasets/Berlin/"
+DATASET="berlin0-10"
+SPEED=5
+
+ESTART=10
+EEND=30
+ESTEP=10
+MSTART=3
+MEND=5
+MSTEP=1
+DSTART=3
+DEND=5
+DSTEP=1
+
+for E in `seq $ESTART $ESTEP $EEND` 
+do
+  for M in `seq $MSTART $MSTEP $MEND` 
+  do
+    for D in `seq $DSTART $DSTEP $DEND` 
+    do
+      spark-submit --class FlockFinderSpatialJoin $JAR_FILE --partitions $PARTITIONS --cores $CORES --speed $SPEED --path $DATASET_PATH --dataset $DATASET --epsilon $E --mu $M --delta $D
+      spark-submit --class FlockFinderMergeLast $JAR_FILE --partitions $PARTITIONS --cores $CORES --speed $SPEED --path $DATASET_PATH --dataset $DATASET --epsilon $E --mu $M --delta $D
+    done
+  done
+done
+
+echo "DONE!!!"
