efb7fe48e07163e5bfa9a71aa64b8c07d8ccc549
aocalderon <andress.calderon@gmail.com>
2018-03-02 22:13:04 -0800

    Fixing bug about redundant and duplicates disks in MergeLast using a reducer...

--- PFlock/src/main/scala/Test.scala
+++ PFlock/src/main/scala/Test.scala
@@ -2,6 +2,11 @@ import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
+import collection.immutable.HashSet
+import SPMF.AlgoFPMax
+import scala.collection.JavaConverters._
+
+
 
 object Test {
   private val logger: Logger = LoggerFactory.getLogger("myLogger")
@@ -32,15 +37,29 @@ object Test {
     result.toList.map(i => list(i))
   }
 
-	def main(args: Array[String]): Unit = {
-		val list: List[Int] = List(0,1,2,3,4,5,6,7,8,9,10)
-    val delta: Int = 5
-    var start: Int = 0
+  def reduceDisks(a: Set[String], b: Set[String]): Set[String] ={
+    val transactions = a.union(b).map { disk => disk.split(" ").map(new Integer(_)).toList.asJava}.toList.asJava
+    val algorithm = new AlgoFPMax
+    val maximals = algorithm.runAlgorithm(transactions, 1)
 
-    while(start + delta <= list.length) {
-      val sublist = list.slice(start, start + delta)
-      logger.info(reorder(sublist).toString())
-      start = start + 1
-    }
+    maximals.getItemsets(1).asScala.map(m => m.asScala.toList.sorted.mkString(" ")).toSet
+  }
+
+	def main(args: Array[String]): Unit = {
+    val d0: Set[String] = HashSet("0 9")
+		val d1: Set[String] = HashSet("1 2 3 4 5")
+    val d2: Set[String] = HashSet("1 3 4 5")
+    val d3: Set[String] = HashSet("2 3 4")
+    val d4: Set[String] = HashSet("3 4 5")
+    val d5: Set[String] = HashSet("6 7 8 9")
+    val d6: Set[String] = HashSet("1 2 3 4 5")
+    val d7: Set[String] = HashSet("7 8")
+    val d8: Set[String] = HashSet("8 9")
+    val d9: Set[String] = HashSet("6 7")
+
+    var disks: List[Set[String]] = List(d0,d1,d2,d3,d4,d5,d6,d7,d8,d9)
+    val result = disks.reduce{ (a, b) => reduceDisks(a, b) }
+
+    result.foreach(println)
 	}
 }
