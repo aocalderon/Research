f7b01762585108bfdbad4be5a8172623e24eb400
aocalderon <andress.calderon@gmail.com>
2018-03-23 19:09:28 -0700

    Still working on Tester3...

--- PFlock/src/main/scala/Tester2.scala
+++ PFlock/src/main/scala/Tester2.scala
@@ -1,4 +1,6 @@
 import org.apache.spark.sql.functions._
+
+import scala.collection.JavaConverters._
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -15,6 +17,8 @@ object Tester2 {
   var axis: String   = ""
   var points: String = ""
   var lines: String  = ""
+  var left: Int = 0
+  var right: Int = 0
   var n: Long = 0
   var m: Long = 0
 
@@ -26,11 +30,16 @@ object Tester2 {
 
   case class Flock(start: Int, end: Int, ids: String, x: Double, y: Double)
 
+  case class Segment(ids: String, segment: List[Int])
+
+  val spark: SparkSession = SparkSession.builder()
+    .master("local[*]")
+    .appName("Tester")
+    .getOrCreate()
+  import spark.implicits._
+  var segments: Dataset[Segment] = spark.sparkContext.emptyRDD[Segment].toDS()
+
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder()
-      .master("local[*]")
-      .appName("Tester")
-      .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
     log.info("SparkSession has been created...")
     import spark.implicits._
@@ -65,39 +74,145 @@ object Tester2 {
     val timestamps = List(4, 0, 2, 3, 1, 8, 6, 7, 5, 10, 9)
 
     var nodes = spark.sparkContext.emptyRDD[Disk].toDS()
-    var links = spark.sparkContext.emptyRDD[Link].toDS()
+    //var links = spark.sparkContext.emptyRDD[Link].toDS()
 
-    val C_0 = T.filter(p => timestamps.head == p.t).map(p => Disk(p.t, getLetter(p.id), p.x, p.y))
+    left = 0
+    right = 4
+    val C_0 = maximals(T, timestamps.head)
     nodes = nodes.union(C_0) // if timestamp is first or last in window
-    val C_1 = T.filter(p => timestamps(1) == p.t).map(p => Disk(p.t, getLetter(p.id), p.x, p.y))
+    val C_1 = maximals(T, timestamps(1))
     nodes = nodes.union(C_1) // if timestamp is first or last in window
-    setAxis(spark, nodes)
-    setPoints(spark, nodes)
+    setAxis(nodes)
+    setPoints(nodes)
     LaTeX.write(parser("Maximal disks"))
 
-    links = links.union(join(spark, nodes, C_1))
-    setLines(spark, links)
+    nodes = nodes.union(join(nodes, C_1)).distinct()
+    setLines(nodes, List(4,0).sorted)
     LaTeX.write(parser("Join"))
+
+    nodes = filter(nodes, List(4,0).sorted)
+    setPoints(nodes)
+    setLines(nodes, List(4,0).sorted)
+    LaTeX.write(parser("Filter"))
+
+    // Timestamp 2
+    val C_2 = maximals(T, timestamps(2))
+    nodes = nodes.union(C_2)
+    setAxis(nodes)
+    setPoints(nodes)
+    LaTeX.write(parser("Maximal disks"))
+
+    nodes = nodes.union(join(nodes, C_2)).distinct()
+    setLines(nodes, List(4,0,2).sorted)
+    LaTeX.write(parser("Join"))
+
+    nodes = filter(nodes, List(4,0,2).sorted)
+    setPoints(nodes)
+    setLines(nodes, List(4,0,2).sorted)
+    LaTeX.write(parser("Filter"))
+
+    // Timestamp 3
+    val C_3 = maximals(T, timestamps(3))
+    nodes = nodes.union(C_3)
+    setAxis(nodes)
+    setPoints(nodes)
+    LaTeX.write(parser("Maximal disks"))
+
+    nodes = nodes.union(join(nodes, C_3)).distinct()
+    setLines(nodes, List(4,0,2,3).sorted)
+    LaTeX.write(parser("Join"))
+
+    nodes = filter(nodes, List(4,0,2,3).sorted)
+    setPoints(nodes)
+    setLines(nodes, List(4,0,2,3).sorted)
+    LaTeX.write(parser("Filter"))
+
+    // Timestamp 4
+    val C_4 = maximals(T, timestamps(4))
+    nodes = nodes.union(C_4)
+    setAxis(nodes)
+    setPoints(nodes)
+    LaTeX.write(parser("Maximal disks"))
+
+    nodes = nodes.union(join(nodes, C_4)).distinct()
+    setLines(nodes, List(4,0,2,3,1).sorted)
+    LaTeX.write(parser("Join"))
+
+    nodes = filter(nodes, List(4,0,2,3,1).sorted)
+    setPoints(nodes)
+    setLines(nodes, List(4,0,2,3,1).sorted)
+    LaTeX.write(parser("Filter"))
+
+    left = 4
+    right = 8
+    // Timestamp 4
+    val C_5 = maximals(T, timestamps(5))
+    nodes = nodes.union(C_5)
+    setAxis(nodes)
+    setPoints(nodes)
+    LaTeX.write(parser("Maximal disks"))
+
+    nodes = nodes.union(join(nodes, C_5)).distinct()
+    setLines(nodes, List(4,0,2,3,1,8).sorted)
+    LaTeX.write(parser("Join"))
+
+    nodes = filter(nodes, List(4,0,2,3,1,8).sorted)
+    setPoints(nodes)
+    setLines(nodes, List(4,0,2,3,1,8).sorted)
+    LaTeX.write(parser("Filter"))
 
     LaTeX.close()
     spark.close
   }
 
-  def join(spark: SparkSession, nodes: Dataset[Disk], C: Dataset[Disk]): Dataset[Link] = {
-    import spark.implicits._
-    nodes.select("t", "ids").toDF("t1", "ids")
-      .join(C.select("t", "ids").toDF("t2", "ids"), "ids")
-      .select("t1", "t2", "ids")
-      .map{ l =>
-        val t1  = l.getInt(0)
-        val t2  = l.getInt(1)
-        val ids = l.getString(2)
-        Link(t1,ids,t2,ids)
-      }.as[Link]
-      .filter(l => s"${l.ids1} ${l.t1}" != s"${l.ids2} ${l.t2}")
+  def maximals(T: Dataset[ST_Point], t: Int): Dataset[Disk] = {
+    T.filter(_.t == t).map(p => Disk(p.t, getLetter(p.id), p.x, p.y)).as[Disk]
+  }
+
+  def join(nodes: Dataset[Disk], C: Dataset[Disk]): Dataset[Disk] = {
+    nodes.select("ids").distinct().join(C, "ids").select("t", "ids", "x", "y").as[Disk]
+  }
+
+  def filter(nodes: Dataset[Disk], timestamps: List[Int]): Dataset[Disk] = {
+    nodes.select("ids", "t")
+      .groupBy("ids")
+      .agg(collect_list("t").alias("times"))
+      .map{ t =>
+        val ids = t.getString(0)
+        val times = t.getList[Int](1).asScala.toList.sorted
+
+        (ids, checkConsecutiveness(times, timestamps))
+      }
+      .toDF("ids", "segments")
+      .withColumn("segment", explode($"segments"))
+      .select("ids", "segment")
+      .filter{ s =>
+        val segment = s.getList[Int](1).asScala.toList
+
+        segment.head == left || segment.last == right
+      }
+      .withColumn("t", explode($"segment"))
+      .select("t", "ids")
+      .join(nodes, Seq("t", "ids"))
+      .select("t", "ids", "x",  "y")
+      .as[Disk]
+  }
+
+  def checkConsecutiveness(times: List[Int], timestamps: List[Int]): Array[Array[Int]] = {
+    if(times.lengthCompare(1) == 0) return Array(times.toArray)
+    times.sliding(2)
+      .map{ m =>
+        if( timestamps.indexOf(m.head) + 1 == timestamps.indexOf(m.last) )
+          s"${m.head} ${m.last}"
+        else
+          s"${m.head};${m.last}"
+      }
+      .mkString(" ").split(";")
+      .map(_.split(" ").map(_.toInt).distinct)
   }
 
   def parser(phase: String): String = {
+    log.info(s"Parsing $phase...")
     val header  = parserHeader(phase)
     val content = s"$axis\n\t\t$points\n\t\t$lines"
     val footer  = parserFooter()
@@ -106,8 +221,7 @@ object Tester2 {
   }
 
   def parserHeader(phase: String): String = {
-    val title = s"$phase..."
-    val section = s"\n\\section*{$title}\n\t\\vspace{1cm}\n"
+    val section = s"\n\\section*{$phase}\n\t\\vspace{1cm}\n"
     val scale = s"\t\\psscalebox{1.0 1.0} {\n"
     val begin = s"\t\\begin{pspicture}(0,0.0)($m,$n)\n\t\t"
 
@@ -118,8 +232,7 @@ object Tester2 {
     "\n\t\\end{pspicture}\n}\n\\clearpage\n"
   }
 
-  def setAxis(spark: SparkSession, nodes: Dataset[Disk]): Unit = {
-    import spark.implicits._
+  def setAxis(nodes: Dataset[Disk]): Unit = {
     axis = nodes.select("t").distinct().orderBy("t")
       .map { t =>
         val x = t.getInt(0)
@@ -132,8 +245,7 @@ object Tester2 {
       .mkString("\n\t\t")
   }
 
-  def setPoints(spark: SparkSession, nodes: Dataset[Disk]): Unit = {
-    import spark.implicits._
+  def setPoints(nodes: Dataset[Disk]): Unit = {
     points = nodes.map { t =>
         val name = s"${t.ids} ${t.t}"
         val coords = s"${t.x},${t.y}"
@@ -143,26 +255,33 @@ object Tester2 {
       .mkString("\n\t\t")
   }
 
-  def setLines(spark: SparkSession, links: Dataset[Link]): Unit = {
-    import spark.implicits._
-    lines = links.map { l =>
-        val node1 = s"${l.ids1} ${l.t1}"
-        val node2 = s"${l.ids2} ${l.t2}"
-        s"\\ncline[linecolor=${getColor(l.ids1)},linestyle=$LineStyle]{$node1}{$node2}"
-      }
-      .collect()
-      .mkString("\n\t\t")
-  }
+  def setLines(nodes: Dataset[Disk], timestamps: List[Int]): Unit = {
+    val t0 = nodes.select("ids","t")
+      .orderBy("ids", "t")
+      .groupBy("ids").agg(collect_list($"t"))
+      .map{ t =>
+        val ids = t.getString(0)
+        val times = t.getList[Int](1).asScala.toList.sorted
 
-  def checkConsecutiveness(dots: List[String], psline: String, times: List[Int]): String = {
-    val lines = dots.map(_.replace("(", "").replace(")", "").split(","))
-      .map(m => (m.head.toDouble, m.last.toDouble))
-      .sliding(2).toList
-      .map(l => if (times.indexOf(l.head._1) + 1 == times.indexOf(l.last._1)) (l.head, l.last) else ((-1, -1), l.last))
-      .sortBy(_._2._1).map(m => List(m._1, m._2)).reduce(_ union _)
-      .mkString(";").split(";\\(-1,-1\\);").map(_.split(";").filter(_ != "(-1,-1)").distinct).filter(_.length > 1)
-      .map(p => s"$psline ${p.mkString("")}")
-    lines.mkString(" ")
+        (ids, checkConsecutiveness(times, timestamps))
+      }
+      .toDF("ids", "segments")
+      .withColumn("segment", explode($"segments"))
+      .select("ids", "segment")
+    t0.orderBy("ids").show(200, truncate = false)
+
+    val t1 = t0.filter(s => s.getList[Int](1).asScala.lengthCompare(1) > 0).flatMap { l =>
+        val ids = l.getString(0)
+        val segments = l.getList[Int](1).asScala.toList.sorted.sliding(2)
+        segments.map{ s =>
+          val node1 = s"$ids ${s.head}"
+          val node2 = s"$ids ${s.last}"
+          s"\\ncline[linecolor=${getColor(ids)},linestyle=$LineStyle]{$node1}{$node2}"
+        }
+      }
+    t1.show(200, truncate = false)
+    lines = t1.collect()
+      .mkString("\n\t\t")
   }
 
   def getColor(ids: String): String = {
