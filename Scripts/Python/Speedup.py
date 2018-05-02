80b66bb78fc49bd01fd1db1d69b8ee2f0c39cd5d
aocalderon <andress.calderon@gmail.com>
2018-05-02 14:20:12 -0700

    Sync...

--- Scripts/Python/Speedup.py
+++ Scripts/Python/Speedup.py
@@ -6,11 +6,11 @@ from datetime import datetime
 
 ## Reading arguments...
 parser = argparse.ArgumentParser()
-parser.add_argument("--min_epsilon",  "-e1",    default=45,                 help="Minimum epsilon...")
+parser.add_argument("--min_epsilon",  "-e1",    default=40,                 help="Minimum epsilon...")
 parser.add_argument("--max_epsilon",  "-e2",    default=50,                 help="Maximum epsilon...")
 parser.add_argument("--step_epsilon", "-se",  default=5,                  help="Step in epsilon...")
-parser.add_argument("--min_mu",       "-m1",    default=5,                  help="Minimum mu...")
-parser.add_argument("--max_mu",       "-m2",    default=5,                  help="Maximum mu...")
+parser.add_argument("--min_mu",       "-m1",    default=4,                  help="Minimum mu...")
+parser.add_argument("--max_mu",       "-m2",    default=4,                  help="Maximum mu...")
 parser.add_argument("--step_mu",      "-sm",  default=1,                  help="Step in mu...")
 parser.add_argument("--min_delta",    "-d1",    default=4,                  help="Minimum delta...")
 parser.add_argument("--max_delta",    "-d2",    default=5,                  help="Maximum delta...")
@@ -53,7 +53,7 @@ def runFlockFinder(cores):
 
 CORES_PER_NODE = 7
 
-for NODES in [1]:
+for NODES in [2]:
   logging.warning("Setting {0} nodes...\n\n".format(NODES))
   setNodes(NODES)
   logging.warning("{0} nodes has been set...\n\n".format(NODES))
