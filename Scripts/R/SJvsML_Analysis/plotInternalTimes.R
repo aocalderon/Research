272c7c7abf1c46c30cd07370c7b0a2066e8f8016
aocalderon <andress.calderon@gmail.com>
2018-05-09 15:22:55 -0700

    Sync...

--- Scripts/R/SJvsML_Analysis/plotInternalTimes.R
+++ Scripts/R/SJvsML_Analysis/plotInternalTimes.R
@@ -4,41 +4,33 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(ggplot2, data.table, foreach, sqldf, tidyr, stringr, dplyr)
 RESEARCH_HOME = Sys.getenv(c("RESEARCH_HOME"))
 PATH = "Scripts/R/SJvsML_Analysis/"
-SAVE_PDF = 
+SAVE_PDF = F
 W = 6
 H = 6
 
-data = read.csv(paste0(RESEARCH_HOME, PATH, 'output.csv'), header = F)
-names(data) = c("Method", "Stage", "Cores", "Epsilon", "Mu", "Delta", "Time", "N")
+data = read.csv(paste0(RESEARCH_HOME, PATH, 'outputStages.csv'), header = F)
+names(data) = c("Method", "Cores", "Epsilon", "Mu", "Delta", "Stage", "Time")
 data$Cores   = as.numeric(as.character(data$Cores))
 data$Epsilon = as.numeric(as.character(data$Epsilon))
 data$Mu      = as.numeric(as.character(data$Mu))
 data$Delta   = as.numeric(as.character(data$Delta))
 data$Time    = as.numeric(as.character(data$Time))
-data$N       = as.numeric(as.character(data$N))
+data = data[data$Time > 0,]
 
-muDefault      = 4
-deltaDefault   = 4
-epsilonDefault = 35
-coresDefault   = 21
+muDefault      = 3
+deltaDefault   = 5
+epsilonDefault = 10
+coresDefault   = 28
 
-dataInternal = data[data$Cores == coresDefault & data$Mu == muDefault & data$Delta == deltaDefault, c("Stage", "Epsilon", "Time", "N")]
-title = "Execution time..."
-g = ggplot(data=dataInternal, aes(x=factor(Epsilon), y=Time, fill=Stage)) +
+dataDelta = data[data$Cores == coresDefault & data$Mu == muDefault & data$Method == "MergeLast" & data$Delta < 7
+                 , c("Epsilon", "Stage", "Time", "Delta")]
+title = "Execution time by delta"
+g = ggplot(data=dataDelta, aes(x=factor(Epsilon), y=Time, fill=Stage)) +
   geom_bar(stat="identity", position=position_dodge(width = 0.75),width = 0.75) +
-  labs(title=title, y="Time(s)", x=expression(paste(epsilon,"(mts)")))
+  labs(title=title, y="Time(s)", x=expression(paste(epsilon,"(mts)"))) +
+  facet_wrap(~Delta)
 if(SAVE_PDF){
-  ggsave("./MLInternalTime.pdf", g)
-} else {
-  plot(g)
-}
- 
-title = "Number of evaluated candidates..."
-g = ggplot(data=dataInternal, aes(x=factor(Epsilon), y=N, fill=Stage)) +
-  geom_bar(stat="identity", position=position_dodge(width = 0.75),width = 0.75) +
-  labs(title=title, y="Time(s)", x=expression(paste(epsilon,"(mts)")))
-if(SAVE_PDF){
-  ggsave("./MLInternalN.pdf", g)
+  ggsave("./MergeLastStagebyDelta.pdf", g)
 } else {
   plot(g)
 }
