59646528b3b55659bd5250ca91da682c4c52793d
aocalderon <andress.calderon@gmail.com>
2018-05-13 11:50:33 -0700

    Sync from DBLab...

--- Scripts/Python/BFEandPFlockRunner.py
+++ Scripts/Python/BFEandPFlockRunner.py
@@ -4,17 +4,17 @@ import logging
 
 ## Reading arguments...
 parser = argparse.ArgumentParser()
-parser.add_argument("--min_epsilon", "-e1", default=40.0, help="Minimum epsilon...")
-parser.add_argument("--max_epsilon", "-e2", default=50.0, help="Maximum epsilon...")
+parser.add_argument("--min_epsilon", "-e1", default=30.0, help="Minimum epsilon...")
+parser.add_argument("--max_epsilon", "-e2", default=30.0, help="Maximum epsilon...")
 parser.add_argument("--step_epsilon", "-se", default=5.0, help="Step in epsilon...")
-parser.add_argument("--min_mu", "-m1", default=3, help="Minimum mu...")
-parser.add_argument("--max_mu", "-m2", default=3, help="Maximum mu...")
+parser.add_argument("--min_mu", "-m1", default=5, help="Minimum mu...")
+parser.add_argument("--max_mu", "-m2", default=5, help="Maximum mu...")
 parser.add_argument("--step_mu", "-sm", default=2, help="Step in mu...")
-parser.add_argument("--min_delta", "-d1", default=3, help="Minimum delta...")
-parser.add_argument("--max_delta", "-d2", default=3, help="Maximum delta...")
+parser.add_argument("--min_delta", "-d1", default=5, help="Minimum delta...")
+parser.add_argument("--max_delta", "-d2", default=5, help="Maximum delta...")
 parser.add_argument("--step_delta", "-sd", default=1, help="Step in delta...")
 parser.add_argument("--path", "-p", default="Datasets/Berlin/", help="URL path...")
-parser.add_argument("--dataset", "-i", default="berlin0-5", help="Point dataset...")
+parser.add_argument("--dataset", "-i", default="berlin0-10", help="Point dataset...")
 args = parser.parse_args()
 
 ## Setting variables...
@@ -34,7 +34,7 @@ for delta in range(min_delta, max_delta + 1, step_delta):
   for epsilon in range(min_epsilon, max_epsilon + 1, step_epsilon):
     for mu in range(min_mu, max_mu + 1, step_mu):
       logging.warning("\n\nIteration Epsilon={0}, Mu={1} and Delta={2} has started...".format(epsilon, mu, delta))
-      command = "{0} -p {1} -i {2} -e {3} -m {4} -d {5}".format(runner_script
+      command = "{0} -p {1} -i {2} -e {3} -m {4} -d {5} --no_bfe".format(runner_script
       , args.path
       , args.dataset
       , epsilon
