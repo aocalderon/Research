2834567470f6902ef4740e96117dd1b7909a8d50
aocalderon <andress.calderon@gmail.com>
2018-05-17 13:34:04 -0700

    Creating separate MergeLast and SpatialJoin benchmarkers...

---
+++ Scripts/Python/NodesSetter.py
@@ -0,0 +1,28 @@
+import subprocess
+import argparse
+import os
+import logging
+from datetime import datetime
+
+## Reading arguments...
+parser = argparse.ArgumentParser()
+parser.add_argument("--nodes", "-n", default=1, help="Number of nodes...")
+args = parser.parse_args()
+
+
+## Setting variables...
+spark_home = os.environ['SPARK_HOME']
+nodes = ['acald013@dblab-rack12', 'acald013@dblab-rack14', 'acald013@dblab-rack11', 'acald013@dblab-rack15']
+
+## Setting nodes...
+def setNodes(n):
+  subprocess.call("{0}/sbin/stop-all.sh".format(spark_home), shell=True)
+  slaves = open("{0}/conf/slaves".format(spark_home), "w")
+  slaves.write("\n".join(nodes[0:n]))
+  slaves.close()
+  subprocess.call("{0}/sbin/start-all.sh".format(spark_home), shell=True)
+
+n = ars.nodes
+logging.warning("Setting {0} nodes...\n\n".format(n))
+setNodes(n)
+logging.warning("{0} nodes has been set...\n\n".format(n))
