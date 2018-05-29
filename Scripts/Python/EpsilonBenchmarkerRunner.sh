e79063a16b1af9a1a8bcd799139232691eadfda1
aocalderon <andress.calderon@gmail.com>
2018-05-29 08:19:37 -0700

    Adding new files...

---
+++ Scripts/Python/EpsilonBenchmarkerRunner.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+./EpsilonBenchmarker.sh 10
+./EpsilonBenchmarker.sh 15
+./EpsilonBenchmarker.sh 20
+./EpsilonBenchmarker.sh 25
+./EpsilonBenchmarker.sh 30
+
+./EpsilonBenchmarker.sh 10
+./EpsilonBenchmarker.sh 15
+./EpsilonBenchmarker.sh 20
+./EpsilonBenchmarker.sh 25
+./EpsilonBenchmarker.sh 30
+
+./EpsilonBenchmarker.sh 10
+./EpsilonBenchmarker.sh 15
+./EpsilonBenchmarker.sh 20
+./EpsilonBenchmarker.sh 25
+./EpsilonBenchmarker.sh 30
+
+./EpsilonBenchmarker.sh 10
+./EpsilonBenchmarker.sh 15
+./EpsilonBenchmarker.sh 20
+./EpsilonBenchmarker.sh 25
+./EpsilonBenchmarker.sh 30
+
+./EpsilonBenchmarker.sh 10
+./EpsilonBenchmarker.sh 15
+./EpsilonBenchmarker.sh 20
+./EpsilonBenchmarker.sh 25
+./EpsilonBenchmarker.sh 30
