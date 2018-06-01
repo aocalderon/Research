16f0c49d5ab10316b1113a5babab4cecf4eb46ba
aocalderon <andress.calderon@gmail.com>
2018-05-31 19:44:58 -0700

    Running a new set of experiments...

--- Scripts/Python/DeltaBenchmarkerRunner.sh
+++ Scripts/Python/DeltaBenchmarkerRunner.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/bash
 
+./DeltaBenchmarker.sh 2
 ./DeltaBenchmarker.sh 3
-./DeltaBenchmarker.sh 4
-./DeltaBenchmarker.sh 5
-./DeltaBenchmarker.sh 6
+#./DeltaBenchmarker.sh 4
+#./DeltaBenchmarker.sh 5
+#./DeltaBenchmarker.sh 6
 
+./DeltaBenchmarker.sh 2
 ./DeltaBenchmarker.sh 3
-./DeltaBenchmarker.sh 4
-./DeltaBenchmarker.sh 5
-./DeltaBenchmarker.sh 6
+#./DeltaBenchmarker.sh 4
+#./DeltaBenchmarker.sh 5
+#./DeltaBenchmarker.sh 6
 
+./DeltaBenchmarker.sh 2
 ./DeltaBenchmarker.sh 3
-./DeltaBenchmarker.sh 4
-./DeltaBenchmarker.sh 5
-./DeltaBenchmarker.sh 6
+#./DeltaBenchmarker.sh 4
+#./DeltaBenchmarker.sh 5
+#./DeltaBenchmarker.sh 6
 
+./DeltaBenchmarker.sh 2
 ./DeltaBenchmarker.sh 3
-./DeltaBenchmarker.sh 4
-./DeltaBenchmarker.sh 5
-./DeltaBenchmarker.sh 6
+#./DeltaBenchmarker.sh 4
+#./DeltaBenchmarker.sh 5
+#./DeltaBenchmarker.sh 6
 
+./DeltaBenchmarker.sh 2
 ./DeltaBenchmarker.sh 3
-./DeltaBenchmarker.sh 4
-./DeltaBenchmarker.sh 5
-./DeltaBenchmarker.sh 6
+#./DeltaBenchmarker.sh 4
+#./DeltaBenchmarker.sh 5
+#./DeltaBenchmarker.sh 6
--- Scripts/Python/MuBenchmarkerRunner.sh
+++ Scripts/Python/MuBenchmarkerRunner.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/bash
 
-./MuBenchmarker.sh 3
-./MuBenchmarker.sh 4
-./MuBenchmarker.sh 5
-./MuBenchmarker.sh 6
+#./MuBenchmarker.sh 3
+#./MuBenchmarker.sh 4
+#./MuBenchmarker.sh 5
+#./MuBenchmarker.sh 6
 
+./MuBenchmarker.sh 7
 ./MuBenchmarker.sh 3
-./MuBenchmarker.sh 4
-./MuBenchmarker.sh 5
-./MuBenchmarker.sh 6
+#./MuBenchmarker.sh 4
+#./MuBenchmarker.sh 5
+#./MuBenchmarker.sh 6
 
+./MuBenchmarker.sh 7
 ./MuBenchmarker.sh 3
-./MuBenchmarker.sh 4
-./MuBenchmarker.sh 5
-./MuBenchmarker.sh 6
+#./MuBenchmarker.sh 4
+#./MuBenchmarker.sh 5
+#./MuBenchmarker.sh 6
 
+./MuBenchmarker.sh 7
 ./MuBenchmarker.sh 3
-./MuBenchmarker.sh 4
-./MuBenchmarker.sh 5
-./MuBenchmarker.sh 6
+#./MuBenchmarker.sh 4
+#./MuBenchmarker.sh 5
+#./MuBenchmarker.sh 6
 
+./MuBenchmarker.sh 7
 ./MuBenchmarker.sh 3
-./MuBenchmarker.sh 4
-./MuBenchmarker.sh 5
-./MuBenchmarker.sh 6
+#./MuBenchmarker.sh 4
+#./MuBenchmarker.sh 5
+#./MuBenchmarker.sh 6
