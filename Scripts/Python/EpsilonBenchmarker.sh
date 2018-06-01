1d632fa6d8510dcbc3fa2d156c7625acefe873bf
aocalderon <andress.calderon@gmail.com>
2018-05-31 19:44:58 -0700

    Running a new set of experiments...

--- Scripts/Python/EpsilonBenchmarker.sh
+++ Scripts/Python/EpsilonBenchmarker.sh
@@ -28,17 +28,17 @@ $SPARK_HOME/sbin/stop-all.sh
 TIMESTAMP=`date`
 echo "FLOCKFINDER=SpatialJoin;TIME=$TIMESTAMP;RUN=$ID;NODES=$NODES;ESTART=$ESTART;EEND=$EEND;ESTEP=$ESTEP;MU=$M;DELTA=$D;SCRIPT=$SCRIPT_NAME;EVENT=End"
 
-##ID=`date +%s`
-##TIMESTAMP=`date`
-##echo "FLOCKFINDER=MergeLast;TIME=$TIMESTAMP;RUN=$ID;NODES=$NODES;ESTART=$ESTART;EEND=$EEND;ESTEP=$ESTEP;MU=$M;DELTA=$D;SCRIPT=$SCRIPT_NAME;EVENT=Start"
-##python3 NodesSetter.py -n $NODES
-##CORES=$(($NODES * $CORES_PER_NODE))
-##spark-submit --class FlockFinderMergeLast $JAR_FILE \
-##--path $DATAPATH --dataset $DATASET \
-##--epsilon $ESTART --epsilon_max $EEND --epsilon_step $ESTEP \
-##--mu $M --mu_max $M --mu_step 1 \
-##--delta $D --delta_max $D --delta_step 1 \
-##--cores $CORES
-##$SPARK_HOME/sbin/stop-all.sh
-##TIMESTAMP=`date`
-##echo "FLOCKFINDER=MergeLast;TIME=$TIMESTAMP;RUN=$ID;NODES=$NODES;ESTART=$ESTART;EEND=$EEND;ESTEP=$ESTEP;MU=$M;DELTA=$D;SCRIPT=$SCRIPT_NAME;EVENT=End"
+ID=`date +%s`
+TIMESTAMP=`date`
+echo "FLOCKFINDER=MergeLast;TIME=$TIMESTAMP;RUN=$ID;NODES=$NODES;ESTART=$ESTART;EEND=$EEND;ESTEP=$ESTEP;MU=$M;DELTA=$D;SCRIPT=$SCRIPT_NAME;EVENT=Start"
+python3 NodesSetter.py -n $NODES
+CORES=$(($NODES * $CORES_PER_NODE))
+spark-submit --class FlockFinderMergeLast $JAR_FILE \
+--path $DATAPATH --dataset $DATASET \
+--epsilon $ESTART --epsilon_max $EEND --epsilon_step $ESTEP \
+--mu $M --mu_max $M --mu_step 1 \
+--delta $D --delta_max $D --delta_step 1 \
+--cores $CORES
+$SPARK_HOME/sbin/stop-all.sh
+TIMESTAMP=`date`
+echo "FLOCKFINDER=MergeLast;TIME=$TIMESTAMP;RUN=$ID;NODES=$NODES;ESTART=$ESTART;EEND=$EEND;ESTEP=$ESTEP;MU=$M;DELTA=$D;SCRIPT=$SCRIPT_NAME;EVENT=End"
---
+++ Scripts/Python/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+./DeltaBenchmarkerRunner.sh
+./MuBenchmarkerRunner.sh
