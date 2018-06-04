7b96fc552e0ecdd22c3b5bf63728bbaa649045c5
aocalderon <andress.calderon@gmail.com>
2018-06-04 09:50:45 -0700

    Adding Mu results from dblab-rack11...

---
+++ Scripts/Misc/BfeBenchmark.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DATASET=$1
+EPSILON=$2
+MU=$3
+DELTA=$4
+SCRIPT_NAME="BfeBenchmark"
+FILENAME=$(basename -- "$DATASET")
+EXTENSION="${FILENAME##*.}"
+FILENAME="${FILENAME%.*}"
+
+ID=`date +%s`
+TIMESTAMP=`date`
+echo "FLOCKFINDER=bfe;TIME=$TIMESTAMP;RUN=$ID;EPSILON=$EPSILON;MU=$MU;DELTA=$DELTA;SCRIPT=$SCRIPT_NAME;EVENT=Start"
+time ~/opt/BFE_Modified/build/flocks -m BFE \
+	-d $EPSILON \
+	-s $MU \
+	-l $DELTA \
+	-f $DATASET > /tmp/bfe.log
+FILENAME="BFE_N${FILENAME}_E${EPSILON}_M${MU}_D${DELTA}.txt"
+cat /tmp/bfe.log | grep -P "\d\, \d+\," > /tmp/$FILENAME
+grep "Closing stream" /tmp/bfe.log
+grep "totalTime" /tmp/bfe.log
+ID=`date +%s`
+TIMESTAMP=`date`
+echo "FLOCKFINDER=bfe;TIME=$TIMESTAMP;RUN=$ID;EPSILON=$EPSILON;MU=$MU;DELTA=$DELTA;SCRIPT=$SCRIPT_NAME;EVENT=End"
+
--- Scripts/Python/EpsilonBenchmarkerRunner.sh
+++ Scripts/Python/EpsilonBenchmarkerRunner.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/bash
 
-./EpsilonBenchmarker.sh 10
+./EpsilonBenchmarker.sh 100
+./EpsilonBenchmarker.sh 150
+./EpsilonBenchmarker.sh 200
 #./EpsilonBenchmarker.sh 15
 #./EpsilonBenchmarker.sh 20
 #./EpsilonBenchmarker.sh 25
 #./EpsilonBenchmarker.sh 30
 
-./EpsilonBenchmarker.sh 10
+#./EpsilonBenchmarker.sh 10
 #./EpsilonBenchmarker.sh 15
 #./EpsilonBenchmarker.sh 20
 #./EpsilonBenchmarker.sh 25
 #./EpsilonBenchmarker.sh 30
 
-./EpsilonBenchmarker.sh 10
+#./EpsilonBenchmarker.sh 10
 #./EpsilonBenchmarker.sh 15
 #./EpsilonBenchmarker.sh 20
 #./EpsilonBenchmarker.sh 25
 #./EpsilonBenchmarker.sh 30
 
-./EpsilonBenchmarker.sh 10
+#./EpsilonBenchmarker.sh 10
 #./EpsilonBenchmarker.sh 15
 #./EpsilonBenchmarker.sh 20
 #./EpsilonBenchmarker.sh 25
 #./EpsilonBenchmarker.sh 30
 
-./EpsilonBenchmarker.sh 10
+#./EpsilonBenchmarker.sh 10
 #./EpsilonBenchmarker.sh 15
 #./EpsilonBenchmarker.sh 20
 #./EpsilonBenchmarker.sh 25
