7a44469265328445a76a33a7f73b3908d42838bc
aocalderon <andress.calderon@gmail.com>
2018-09-27 22:04:34 -0700

    Tracking LCM performance...

--- Scripts/Python/LCMUno_vs_LCMSpmf.py
+++ Scripts/Python/LCMUno_vs_LCMSpmf.py
@@ -1,17 +1,25 @@
 #!/home/acald013/opt/miniconda3/bin/python
 
 import os
+import argparse
 from subprocess import call
 import logging
+
+## Reading arguments...
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_file", "-i", default=1, help="Input file...")
+args = parser.parse_args()
 
 logging.basicConfig(format="%(asctime)s -> LOG %(message)s")
 research_home  = os.environ['RESEARCH_HOME']
 input_path     = "Validation/LCM_max/input"
-input_file     = "Datasets_berlin0-0_110.0_5_1_-1.txt" 
+#input_file     = "Datasets_berlin0-10_110.0_5_6_0.txt" 
+input_file     = args.input_file
 input_url      = "{}/{}/{}".format(research_home, input_path, input_file)
-lcm_scala      = "Scripts/Scala/LCM/target/scala-2.11/lcm_2.11-0.1.jar"
-lcm_path       = "{}/{}".format(research_home, lcm_scala)
-debug          = True
+lcm_java       = "PFlock/target/scala-2.11/pflock_2.11-2.0.jar"
+lcm_class      = "--class SPMF.LCM"
+lcm_path       = "{}/{}".format(research_home, lcm_java)
+debug          = False
 
 logging.warning("Extracting datasets...")
 tests = []
@@ -25,15 +33,16 @@ for line in f.readlines():
     if old_id != cur_id:
         test_in.close()
         test_name = "{}/{}/LCMinput_{}.txt".format(research_home, input_path, cur_id)
-        print("Saving {}...".format(test_name))
+        # print("Saving {}...".format(test_name))
         test_in = open(test_name,'w')
         tests.append(test_name)
         old_id = cur_id
     test_in.write(cur_line + '\n')
 test_in.close()
 logging.warning("Done!!!")
+logging.warning("{} has been read...".format(input_file))
 
-tests = tests[0:42]
+# tests = tests[0:3]
 for test in tests:
     logging.warning("Running LCMuno...")
     fid = test.split("/")[-1].split("_")[-1].split(".")[0]
@@ -55,23 +64,24 @@ for test in tests:
     output_sort.close()
     logging.warning("Done!!!")
     
-    logging.warning("Running LCMand...")
+    logging.warning("Running LCMjava...")
     output_path    = "Validation/LCM_max/output"
-    output_file    = "LCMand_{}.txt".format(fid)
+    output_file    = "LCMjava_{}.txt".format(fid)
     lcmand_url     = "{}/{}/{}".format(research_home, output_path, output_file)
+    command        = "{} {} {} {} {}".format('spark-submit', lcm_class, lcm_path, test, lcmand_url)
     if(debug):
-        print("{} {} {} {}".format('scala', lcm_path, test, lcmand_url))
-    call(['scala', lcm_path, test, lcmand_url])
+        print(command)
+    call([command], shell=True)
     logging.warning("Done!!!")
     
     logging.warning("Sorting files...")
-    lcmand_sorted  = "{}/{}/{}".format(research_home, output_path, "LCM_and_sort_{}.txt".format(fid))
-    lcmuno_sorted  = "{}/{}/{}".format(research_home, output_path, "LCM_uno_sort_{}.txt".format(fid))
+    lcmjava_sorted  = "{}/{}/{}".format(research_home, output_path, "LCM_java_sort_{}.txt".format(fid))
+    lcmuno_sorted   = "{}/{}/{}".format(research_home, output_path, "LCM_uno_sort_{}.txt".format(fid))
     call(['sort', lcmuno_url, '-o', lcmuno_sorted])
-    call(['sort', lcmand_url, '-o', lcmand_sorted])
+    call(['sort', lcmand_url, '-o', lcmjava_sorted])
     logging.warning("Done!!!")
 
     logging.warning("Applying Diff...")
-    call(['diff', '-s', lcmuno_sorted, lcmand_sorted])
+    call(['diff', '-s', lcmuno_sorted, lcmjava_sorted])
     logging.warning("Done!!!")
     
