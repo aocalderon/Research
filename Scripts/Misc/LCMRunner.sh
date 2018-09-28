9946c245dd21e5160325225e7479468a8dfed7ca
aocalderon <andress.calderon@gmail.com>
2018-09-27 22:04:34 -0700

    Tracking LCM performance...

---
+++ Scripts/Misc/LCMRunner.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+python LCMUno_vs_LCMSpmf.py -i Datasets_berlin0-10_110.0_5_6_0.txt
+python LCMUno_vs_LCMSpmf.py -i Datasets_berlin0-10_110.0_5_6_1.txt
+python LCMUno_vs_LCMSpmf.py -i Datasets_berlin0-10_110.0_5_6_2.txt
+python LCMUno_vs_LCMSpmf.py -i Datasets_berlin0-10_110.0_5_6_3.txt
+python LCMUno_vs_LCMSpmf.py -i Datasets_berlin0-10_110.0_5_6_4.txt
