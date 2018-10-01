28c04d9fb0fe648a691f4b9aa1a73839c2168e80
Andres Calderon <andress.calderon@gmail.com>
2018-10-01 10:50:34 -0700

    Working on LCM performance plotter...

--- Scripts/R/plotLCMPerformance.R
+++ Scripts/R/plotLCMPerformance.R
@@ -1,30 +1,22 @@
 #!/usr/bin/Rscript
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(ggplot2, data.table, foreach, sqldf, tidyr, stringr, dplyr, anytime)
+pacman::p_load(ggplot2, stringr)
 RESEARCH_HOME = Sys.getenv(c("RESEARCH_HOME"))
 
-dataFile = paste0(RESEARCH_HOME, 'Scripts/Python/nohup.out')
-
-lines = readLines(dataFile)
-records = c()
-method = ""
-epsilon = 0
-mu = 0
-delta = 0
-stage = ""
-time = 0
+dataFile = paste0(RESEARCH_HOME, 'Validation/LCM_max/nohup.csv')
 readData = TRUE
-uno_time_end_flag = FALSE
 
 if(readData){
-  for(line in lines){
-    if(grepl("LOG Running LCMuno...", line, perl = T)){
-      uno_time_start = str_split_fixed(line, "->", 2)[1]
-      uno_time_end_flag = TRUE
-    } else if(uno_time_end_flag){
-      uno_time_end = str_split_fixed(line, "->", 2)[1]
-      uno_time_end_flag = FALSE
-    }
-  }
+  lines = readLines(dataFile)
+  print(lines)
+  data = as.data.frame(str_split_fixed(lines, ",", 3))
+  names(data) = c("Dataset", "Method", "Time")
 }
+data$Time = as.numeric(levels(data$Time))[data$Time]
+nDataset = nlevels(data$Dataset)
+data$ID = rep(1:nDataset, each = 3)
+
+data = data[40 < data$ID & data$ID < 45,]
+g = ggplot(data=data, aes(x=ID, y=Time, color=Method)) + geom_line()
+plot(g)
