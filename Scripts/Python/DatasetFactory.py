15e37ad6d5d8cef0ce25dee6be50af923ac0ccc2
aocalderon <andress.calderon@gmail.com>
2018-10-26 11:05:11 -0700

    Working in new datasets...

--- Scripts/Python/DatasetFactory.py
+++ Scripts/Python/DatasetFactory.py
@@ -7,11 +7,14 @@ Created on Thu Oct 25 18:36:13 2018
 """
 import pandas as pd
 
-i = 9
-path = "/home/and/Documents/PhD/Research/Validation/LCM_max/input/"
-input_file = "{}Datasets_berlin0-10_110.0_5_6_{}.txt".format(path, i)
-dataset = pd.read_csv(input_file, header=None, names=['id','items'])
-results2 = "/tmp/results_sequential.txt"
+i          = 0
+path       = "~/Research/Validation/LCM_max/input/"
+tag        = "B_T4_E100_M5_D3_t"
+extension  = "txt"
+input_file = "{}{}{}.{}".format(path, tag, i, extension)
+dataset    = pd.read_csv(input_file, header=None, names=['id','items'])
+results2   = "/tmp/results_sequential.txt"
+
 D = pd.Series(dataset['items']).drop_duplicates()
 data = "{}d{}.txt".format(path, i)
 f = open(data, 'w')
