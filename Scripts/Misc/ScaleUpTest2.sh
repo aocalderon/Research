6bd1923a00ddea654e43f779f5afad3d175d1dd0
aocalderon <andress.calderon@gmail.com>
2019-02-22 10:43:26 -0800

    Adding more experiments.  Tuning Spark monitor...

---
+++ Scripts/Misc/ScaleUpTest2.sh
@@ -0,0 +1,17 @@
+spark-submit --class FF /home/acald013/Research/GeoSpark/target/scala-2.11/pflock_2.11-0.1.0.jar --input /home/acald013/Research/Datasets/Berlin/berlin_N60K_T0-5.tsv --epsilon 110 --mu 3 --delta 3 --distance 100 --cores 4 --executors 3
+
+#
+
+spark-submit --class FF /home/acald013/Research/GeoSpark/target/scala-2.11/pflock_2.11-0.1.0.jar --input /home/acald013/Research/Datasets/Berlin/berlin_N60K_T0-5.tsv --epsilon 110 --mu 3 --delta 3 --distance 100 --cores 4 --executors 3
+
+#
+
+spark-submit --class FF /home/acald013/Research/GeoSpark/target/scala-2.11/pflock_2.11-0.1.0.jar --input /home/acald013/Research/Datasets/Berlin/berlin_N60K_T0-5.tsv --epsilon 110 --mu 3 --delta 3 --distance 100 --cores 4 --executors 3
+
+#
+
+spark-submit --class FF /home/acald013/Research/GeoSpark/target/scala-2.11/pflock_2.11-0.1.0.jar --input /home/acald013/Research/Datasets/Berlin/berlin_N60K_T0-5.tsv --epsilon 110 --mu 3 --delta 3 --distance 100 --cores 4 --executors 3
+
+#
+
+spark-submit --class FF /home/acald013/Research/GeoSpark/target/scala-2.11/pflock_2.11-0.1.0.jar --input /home/acald013/Research/Datasets/Berlin/berlin_N60K_T0-5.tsv --epsilon 110 --mu 3 --delta 3 --distance 100 --cores 4 --executors 3
--- Scripts/Python/monitor.py
+++ Scripts/Python/monitor.py
@@ -24,39 +24,62 @@ def main(stdscr):
     c = 0
     while c != ord('q'):
         c = stdscr.getch()
+        # Plot some data...
+        try:
+            response = requests.get("http://localhost:4040/api/v1/applications/{}/executors".format(appID))
+            spark = json.loads(response.text)
+        except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError):
+            continue
         # Clear out anything else the user has typed in
         curses.flushinp()
         stdscr.clear()
-        # Plot some data...
-        response = requests.get("http://localhost:4040/api/v1/applications/{}/executors".format(appID))
-        spark = json.loads(response.text)
 
-        id = 0
-        if spark[id]['id'] == 'driver':
-            id = 1
-        cores = spark[id]['totalCores']
-        tasks = spark[id]['activeTasks']
+        i = 0
+        if spark[i]['id'] == 'driver':
+            i = 1
+        cores = spark[i]['totalCores']
+        tasks = spark[i]['activeTasks']
         if tasks > cores:
             tasks = cores
         series0.append(tasks)
         if len(set(series0)) == 1:
-            series0.append(set(series0).pop() + 1)
-        stdscr.addstr(0, 0, "Executor {} at {}".format(id, spark[id]['hostPort']))
-        stdscr.addstr(1, 0, plot(series0))
-        stdscr.addstr(1 + max(series0) - tasks, width + 11, str(tasks))
+            value = set(series0).pop()
+            if value == cores:
+                value = value - 1
+            else:
+                value = value + 1
+            series0.append(value)
+        id = spark[i]['id']
+        hostPort = spark[i]['hostPort']
+        totalTasks = spark[i]['totalTasks']
+        totalDuration   = round((spark[i]['totalDuration'])/(float(cores)*1000.0), 2)
+        totalInputBytes = round(spark[i]['totalInputBytes']/(1024.0*1024.0), 2)
+        stdscr.addstr(0*gap, 0, "Executor {} at {}\tTasks completed: {}\t Duration: {}s\tInput:{}MB".format(id, hostPort, totalTasks, totalDuration, totalInputBytes))
+        stdscr.addstr(0*gap+1, 0, plot(series0))
+        stdscr.addstr(0*gap+1 + max(series0) - tasks, width + 11, str(tasks))
         series0.reverse()
         series0 = series0[0:width]
         series0.reverse()
 
         if len(spark) > 2:
-            id = 2
-            tasks = spark[id]['activeTasks']
+            i = 2
+            tasks = spark[i]['activeTasks']
             if tasks > cores:
                 tasks = cores
             series1.append(tasks)
             if len(set(series1)) == 1:
-                series1.append(set(series1).pop() + 1)
-            stdscr.addstr(gap, 0, "Executor {} at {}".format(id, spark[id]['hostPort']))
+                value = set(series1).pop()
+                if value == cores:
+                    value = value - 1
+                else:
+                    value = value + 1
+                series1.append(value)
+            id = spark[i]['id']
+            hostPort = spark[i]['hostPort']
+            totalTasks = spark[i]['totalTasks']
+            totalDuration   = round((spark[i]['totalDuration'])/(float(cores)*1000.0), 2)
+            totalInputBytes = round(spark[i]['totalInputBytes']/(1024.0*1024.0), 2)
+            stdscr.addstr(1*gap, 0, "Executor {} at {}\tTasks completed: {}\t Duration: {}s\tInput:{}MB".format(id, hostPort, totalTasks, totalDuration, totalInputBytes))
             stdscr.addstr(gap+1, 0, plot(series1))
             stdscr.addstr(gap+1 + max(series1) - tasks, width + 11, str(tasks))
             series1.reverse()
@@ -64,14 +87,24 @@ def main(stdscr):
             series1.reverse()
 
         if len(spark) > 3:
-            id = 3
-            tasks = spark[id]['activeTasks']
+            i = 3
+            tasks = spark[i]['activeTasks']
             if tasks > cores:
                 tasks = cores
             series2.append(tasks)
             if len(set(series2)) == 1:
-                series2.append(set(series2).pop() + 1)
-            stdscr.addstr(2*gap, 0, "Executor {} at {}".format(id, spark[id]['hostPort']))
+                value = set(series2).pop()
+                if value == cores:
+                    value = value - 1
+                else:
+                    value = value + 1
+                series2.append(value)
+            id = spark[i]['id']
+            hostPort = spark[i]['hostPort']
+            totalTasks = spark[i]['totalTasks']
+            totalDuration   = round((spark[i]['totalDuration'])/(float(cores)*1000.0), 2)
+            totalInputBytes = round(spark[i]['totalInputBytes']/(1024.0*1024.0), 2)
+            stdscr.addstr(2*gap, 0, "Executor {} at {}\tTasks completed: {}\t Duration: {}s\tInput:{}MB".format(id, hostPort, totalTasks, totalDuration, totalInputBytes))
             stdscr.addstr(2*gap+1, 0, plot(series2))
             stdscr.addstr(2*gap+1 + max(series2) - tasks, width + 11, str(tasks))
             series2.reverse()
@@ -79,8 +112,11 @@ def main(stdscr):
             series2.reverse()
     
         # Stages...
-        response = requests.get("http://localhost:4040/api/v1/applications/{}/stages".format(appID))
-        stages   = json.loads(response.text)
+        try:
+            response = requests.get("http://localhost:4040/api/v1/applications/{}/stages".format(appID))
+            stages = json.loads(response.text)
+        except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError):
+            continue
         complete = 0
         active   = 0
         pending  = 0
@@ -95,8 +131,11 @@ def main(stdscr):
                 stageId  = stage['stageId']
                 completeTasks = stage['numCompleteTasks']
                 activeTasks   = stage['numActiveTasks']
-                response = requests.get("http://localhost:4040/api/v1/applications/{}/stages/{}".format(appID, stageId))
-                tasks = json.loads(response.text)
+                try:
+                    response = requests.get("http://localhost:4040/api/v1/applications/{}/stages/{}".format(appID, stageId))
+                    tasks = json.loads(response.text)
+                except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError):
+                    continue
                 totalTasks = len(tasks[0]['tasks'])
                 name = tasks[0]['name']
 
@@ -106,13 +145,13 @@ def main(stdscr):
             complete, total = parse('http://localhost:4040/stages').getroot().get_element_by_id("activeStage-table").cssselect("div span")[1].text.strip().split("/")
             completeTasks = float(complete)
             totalTasks    = float(total)
-            tasksBar = ProgressBar(total=100, prefix='Tasks', suffix="{}/{}".format(complete, total), length=40, fill='#', zfill=' ')
+            tasksBar = ProgressBar(total=100, prefix='Tasks ', suffix="{}/{}".format(complete, total), length=40, fill='#', zfill=' ')
             stdscr.addstr(3*gap+3, 0, "[{}] {}".format(stageId, name))
             stdscr.addstr(3*gap+4, 0, tasksBar.generate_pbar((completeTasks*100.0)/totalTasks))
-        except (IndexError, KeyError):
+        except (IndexError, KeyError, OSError, AttributeError):
             pass
         
         # Wait 1/2 of a second. Read below to learn about how to avoid problems with using time.sleep with getch!
-        time.sleep(0.25)
+        time.sleep(0.01)
         
 wrapper(main)
