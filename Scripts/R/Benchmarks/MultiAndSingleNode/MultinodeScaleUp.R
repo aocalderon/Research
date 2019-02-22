db001936e49286859d23624437e53cf1c9b227f6
aocalderon <andress.calderon@gmail.com>
2019-02-22 10:44:32 -0800

    Plotting multinode experients...

--- Scripts/R/Benchmarks/MultiAndSingleNode/MultinodeScaleUp.R
+++ Scripts/R/Benchmarks/MultiAndSingleNode/MultinodeScaleUp.R
@@ -9,19 +9,19 @@ SAVE_PDF      = T
 SEP           = ";"
 RESEARCH_HOME = Sys.getenv(c("RESEARCH_HOME"))
 
-dataFile = paste0(RESEARCH_HOME, 'Scripts/R/Benchmarks/MultiAndSingleNode/scaleup.txt')
+dataFile = paste0(RESEARCH_HOME, 'Scripts/Misc/MultinodeScaleUp.txt')
 
 data = readLines(dataFile)
 
 data = as.tibble(as.data.frame(data), stringAsFactors = F) %>% 
   rename(Line = data) %>% 
   filter(grepl("PFLOCK;", Line)) %>% 
-  separate(Line, c("Bogus", "Nodes", "Epsilon", "Mu", "Delta", "Time", "Load"), sep = SEP) %>%
-  select(Nodes, Epsilon, Time, Load) %>%
-  mutate(Epsilon = as.numeric(Epsilon), Time = as.numeric(Time), Load = as.numeric(Load)) %>%
-  group_by(Nodes, Epsilon) %>% summarise(Time = mean(Time))
+  separate(Line, c("Bogus", "Cores", "Nodes", "Epsilon", "Mu", "Delta", "Time", "Load", "Id"), sep = ";") %>%
+  select(Nodes, Epsilon, Time) %>%
+  mutate(Epsilon = as.numeric(Epsilon), Time2 = as.numeric(Time)) %>%
+  group_by(Nodes, Epsilon) %>% summarise(Time = mean(Time2), SD = sd(Time2))
 
-title = "Multinode Scale up [berlin_N20K-80K]"
+title = "Multinode Scale up [berlin_N40K-80K]"
 g = ggplot(data=data, aes(x=factor(Epsilon), y=Time, fill=Nodes)) +
   geom_bar(stat="identity", position=position_dodge(width = 0.75),width = 0.75) +
   labs(title=title, y="Time(s)", x=expression(paste(epsilon,"(mts)"))) 
