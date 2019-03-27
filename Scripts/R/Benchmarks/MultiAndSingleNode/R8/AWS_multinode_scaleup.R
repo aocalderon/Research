0626065fa586dd9573194df293eb548a3ce95245
Andres Calderon <andress.calderon@gmail.com>
2019-03-26 20:53:18 -0700

    Adding new plots for scale up...

---
+++ Scripts/R/Benchmarks/MultiAndSingleNode/R8/AWS_multinode_scaleup.R
@@ -0,0 +1,33 @@
+#!/usr/bin/Rscript
+
+require(ggplot2)
+require(stringr)
+require(tidyverse)
+
+READ_DATA     = T
+SAVE_PDF      = F
+SEP           = ";"
+RESEARCH_HOME = Sys.getenv(c("RESEARCH_HOME"))
+RESULTS_PATH = "Scripts/R/Benchmarks/MultiAndSingleNode/R8/"
+RESULTS_NAME = "AWS_multinode_scaleup"
+dataFile = paste0(RESEARCH_HOME, RESULTS_PATH, RESULTS_NAME, '.txt')
+
+data = readLines(dataFile)
+
+data = as.tibble(as.data.frame(data), stringAsFactors = F) %>% 
+  rename(Line = data) %>% 
+  filter(grepl("PFLOCK;", Line)) %>% 
+  separate(Line, c("Bogus", "Cores", "Nodes", "Epsilon", "Mu", "Delta", "Time", "Load", "Id"), sep = ";") %>%
+  select(Nodes, Epsilon, Time) %>%
+  mutate(Epsilon = as.numeric(Epsilon), Time2 = as.numeric(Time)) %>%
+  group_by(Nodes, Epsilon) %>% summarise(Time = mean(Time2), SD = sd(Time2))
+
+title = "Multinode Scale up [Grid, Berlin_N40K, Berlin_N80K, Berlin_N120K, Berlin_N160K]"
+g = ggplot(data=data, aes(x=factor(Epsilon), y=Time, fill=Nodes)) +
+  geom_bar(stat="identity", position=position_dodge(width = 0.75),width = 0.75) +
+  labs(title=title, y="Time(s)", x=expression(paste(epsilon,"(mts)"))) 
+if(SAVE_PDF){
+  ggsave(paste0(RESEARCH_HOME, RESULTS_PATH, RESULTS_NAME, '.pdf'), width = 8, height = 5, dpi = 300, units = "in", device='pdf', g)
+} else {
+  plot(g)
+}
--- Scripts/R/Benchmarks/MultiAndSingleNode/R8/FFTester.R
+++ Scripts/R/Benchmarks/MultiAndSingleNode/R8/FFTester.R
@@ -6,7 +6,7 @@ require(stringr)
 require(tidyverse)
 
 READ_DATA     = T
-SAVE_PDF      = F
+SAVE_PDF      = T
 SEP           = ";"
 RESEARCH_HOME = Sys.getenv(c("RESEARCH_HOME"))
 RESULTS_PATH = "Scripts/R/Benchmarks/MultiAndSingleNode/R8/"
@@ -22,23 +22,19 @@ data = as_tibble(as.data.frame(data), stringAsFactors = F) %>%
   select(Partitions, Time1, Time2) %>%
   mutate(Time1 = as.numeric(Time1), Time2 = as.numeric(Time2)) 
 
-dataTime1 = data %>% filter(Time1 > 0) 
+# dataTime1 = data %>% filter(Time1 > 0) 
   
 title = "Execution time by number of partitions [FF: Flock finder, MF:Maximal finder]"
-t1 = ggplot(data=dataTime1, aes(x=Partitions, y=Time1, group=1)) +
-  geom_line(color="red") +
-  geom_point() + theme(axis.text.x = element_text(hjust=-1, vjust=0.2, angle=90)) +
-  labs(title=title, y="Time(s)", x="Partitions [FF-MF]") +
-  geom_label_repel(aes(label = Time1), data = subset(dataTime1, Time1 < 283))
-
-#g = ggplot(data=data, aes(x=factor(Epsilon), y=Time, fill=Cores)) +
-#  geom_bar(stat="identity", position=position_dodge(width = 0.75),width = 0.75) +
-#  labs(title=title, y="Time(s)", x=expression(paste(epsilon,"(mts)"))) 
-if(SAVE_PDF){
-  ggsave(paste0(RESEARCH_HOME, RESULTS_PATH, RESULTS_NAME, '_T1.pdf'), width = 20, height = 5, dpi = 300, units = "in", device='pdf', t1)
-} else {
-  plot(t1)
-}
+# t1 = ggplot(data=dataTime1, aes(x=Partitions, y=Time1, group=1)) +
+#   geom_line(color="red") +
+#   geom_point() + theme(axis.text.x = element_text(hjust=-1, vjust=0.2, angle=90)) +
+#   labs(title=title, y="Time(s)", x="Partitions [FF-MF]") +
+#   geom_label_repel(aes(label = Time1), data = subset(dataTime1, Time1 < 283))
+# if(SAVE_PDF){
+#   ggsave(paste0(RESEARCH_HOME, RESULTS_PATH, RESULTS_NAME, '_T1.pdf'), width = 20, height = 5, dpi = 300, units = "in", device='pdf', t1)
+# } else {
+#   plot(t1)
+# }
 
 dataTime2 = data %>% filter(Time2 > 0) 
 
@@ -49,7 +45,7 @@ t2 = ggplot(data=dataTime2, aes(x=Partitions, y=Time2, group=1)) +
   geom_label_repel(aes(label = Time2), data = subset(dataTime2, Time2 < 560))
 
 if(SAVE_PDF){
-  ggsave(paste0(RESEARCH_HOME, RESULTS_PATH, RESULTS_NAME, '_T2.pdf'), width = 20, height = 5, dpi = 300, units = "in", device='pdf', t2)
+  ggsave(paste0(RESEARCH_HOME, RESULTS_PATH, RESULTS_NAME, '.pdf'), width = 20, height = 5, dpi = 300, units = "in", device='pdf', t2)
 } else {
   plot(t2)
 }
