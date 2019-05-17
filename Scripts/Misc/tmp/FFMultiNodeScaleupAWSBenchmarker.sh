0098c72fa925376f021f07268221960fac1b9f5e
Andres Calderon <acald013@ucr.edu>
2019-05-17 08:03:53 -0700

    Updating FF and scripts...

--- Scripts/Misc/tmp/FFMultiNodeScaleupAWSBenchmarker.sh
+++ Scripts/Misc/tmp/FFMultiNodeScaleupAWSBenchmarker.sh
@@ -8,6 +8,7 @@ DATASET_PATH="/home/acald013/Research/Datasets/Berlin/SideBySide/"
 DATASET_PREFIX="B"
 MF_PARTITIONS=30
 FF_PARTITIONS=30
+D_PARTITIONS=30
 DATASET_EXT=".tsv"
 JAR="/home/acald013/Research/GeoSpark/target/scala-2.11/pflock_2.11-0.1.jar"
 MASTER="169.235.27.138"
@@ -28,9 +29,11 @@ for n in `seq 1 $N`; do
 	xff=$((${EXECUTORS_SET[e]} * FF_PARTITIONS))
 	ymf=$MF_PARTITIONS
 	xmf=$((${EXECUTORS_SET[e]} * MF_PARTITIONS))
+	yd=$D_PARTITIONS
+	xd=$((${EXECUTORS_SET[e]} * D_PARTITIONS))
 	for((i=0;i<${#EPSILONS[@]};i++)); do
 	    echo "spark-submit --class FF $JAR --input ${DATASET_PATH}${DATASET_NAME}${DATASET_EXT} --epsilon ${EPSILONS[i]} --mu $MU --delta $DELTA --distance ${DISTANCES[i]} --master $MASTER --cores $CORES --executors ${EXECUTORS_SET[e]} --spatial CUSTOM --customymf $ymf --customxmf $xmf --customy $yff --customx $xff --fftimestamp $TIMESTAMP"
-	    spark-submit --class FF $JAR --input ${DATASET_PATH}${DATASET_NAME}${DATASET_EXT} --epsilon ${EPSILONS[i]} --mu $MU --delta $DELTA --distance ${DISTANCES[i]} --master $MASTER --cores $CORES --executors ${EXECUTORS_SET[e]} --spatial CUSTOM --customymf $ymf --customxmf $xmf --customy $yff --customx $xff  --fftimestamp $TIMESTAMP
+	    spark-submit --class FF $JAR --input ${DATASET_PATH}${DATASET_NAME}${DATASET_EXT} --epsilon ${EPSILONS[i]} --mu $MU --delta $DELTA --distance ${DISTANCES[i]} --master $MASTER --cores $CORES --executors ${EXECUTORS_SET[e]} --spatial CUSTOM --mfcustomy $ymf --mfcustomx $xmf --ffcustomy $yff --ffcustomx $xff --dcustomy $yd --dcustomx $xd --fftimestamp $TIMESTAMP
 	done
     done
 done
