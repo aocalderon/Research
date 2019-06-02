79324d24d576dabfb82cf50c94eb0d24dd06fb8c
Andres Calderon <andress.calderon@gmail.com>
2019-06-02 12:08:02 -0700

    Working in bash files to set AWS workers...

---
+++ Docs/setWorker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+worker_number=$1
+
+# Download JDK...
+wget http://www.cs.ucr.edu/~acald013/public/tmp/jdk.tar.gz
+tar -zxvf jdk.tar.gz
+echo "export JAVA_HOME=/home/ubuntu/jdk1.8.0_202" >> ~/.bashrc
+echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc
+
+# Download Spark...
+mkdir Spark
+cd Spark
+wget http://www.cs.ucr.edu/~acald013/public/tmp/spark.tar.gz
+tar -zxvf spark.tar.gz
+echo "export SPARK_HOME=/home/ubuntu/Spark/2.4" >> ~/.bashrc
+echo "export PATH=\$PATH:\$SPARK_HOME/bin" >> ~/.bashrc
+
+# Reload environment variables...
+source ~/.bashrc
+
+# Change content of spark-env.sh in worker accordingly...
+echo "export SPARK_LOCAL_IP=master${worker_number}" >> /home/ubuntu/Spark/2.4/conf/spark-env.sh
+
