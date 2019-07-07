2220ab2c297f98b3d01d6e88eecfe40c3b47972d
Andres Calderon <acald013@ucr.edu>
2019-07-07 10:23:46 -0700

    Fix minor issues...

--- GeoSpark/src/main/scala/MF.scala
+++ GeoSpark/src/main/scala/MF.scala
@@ -439,7 +439,7 @@ object MF{
       val f = new java.io.PrintWriter(s"${params.output()}app-${app_count}_info.tsv")
       f.write(InfoTracker.getExectutorsInfo())
       f.write(InfoTracker.getStagesInfo())
-      f.write(InfoTracker.getTasksInfo())
+      f.write(InfoTracker.getTasksInfoByDuration(25))
       f.close()
     }
     spark.close()
