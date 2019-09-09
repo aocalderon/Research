736529cd53c5568a8217d4f265ee342efcf754f0
Andres Calderon <acald013@ucr.edu>
2019-09-09 08:48:43 -0700

    Finding disks after dbscan...

--- Scripts/Scala/DBSCAN/src/main/scala/DBScanOnSpark.scala
+++ Scripts/Scala/DBSCAN/src/main/scala/DBScanOnSpark.scala
@@ -18,6 +18,39 @@ object DBScanOnSpark {
   private val precision: Double = 0.0001
   private var startTime: Long = System.currentTimeMillis()
 
+  case class ST_Point(tid: Int, x: Double, y: Double, t: Int) extends Ordered[ST_Point]{
+    def distance(other: ST_Point): Double = {
+      math.sqrt(math.pow(this.x - other.x, 2) + math.pow(this.y - other.y, 2))
+    }
+
+    def getJTSPoint: Point = {
+      val point = geofactory.createPoint(new Coordinate(this.x, this.y))
+      point.setUserData(s"${this.tid}\t${this.t}")
+      point
+    }
+
+    override def compare(that: ST_Point): Int = {
+      if (x == that.x) y compare that.y
+      else x compare that.x
+    }
+
+    def canEqual(a: Any) = a.isInstanceOf[ST_Point]
+
+    override def equals(that: Any): Boolean =
+      that match {
+        case that: ST_Point => {
+          that.canEqual(this) && this.x == that.x && this.y == that.y
+        }
+        case _ => false
+      }
+
+    override def toString: String = s"$tid\t$x\t$y\t$t\n"
+  }
+
+  implicit class Crossable[X](xs: Traversable[X]) {
+    def cross[Y](ys: Traversable[Y]) = for { x <- xs; y <- ys } yield (x, y)
+  }
+
   def round3(n: Double): Double = { math.round( n * 1000) / 1000 }
 
   def clocktime = System.currentTimeMillis()
@@ -28,6 +61,7 @@ object DBScanOnSpark {
 
   def main(args: Array[String]): Unit = {
     val params = new DBScanOnSparkConf(args)
+    val debug = params.debug()
     val input = params.input()
     val output = params.output()
     val cores = params.cores()
@@ -115,16 +149,73 @@ object DBScanOnSpark {
       da.setT(t)
       da
     }.collect().toList.asJava
-    val clusters = algo.run(data, minPts, epsilon)
-    clusters.asScala.zipWithIndex.map{ case (cluster, i) =>
-      val xs = cluster.getVectors.asScala.map(_.get(0)).reduce(_ + _)
-      val ys = cluster.getVectors.asScala.map(_.get(1)).reduce(_ + _)
-      val count = cluster.getVectors.size().toDouble
-
-      s"Cluster's centroid ${i} (${xs/count}, ${ys/count})"
-    }.foreach(println)
-    algo.printStatistics()
+    val clusters = algo.run(data, minPts, epsilon).asScala.toList
+    val nClusters = clusters.size
     log(stage, timer, 0, "END")
+
+    if(debug){
+      clusters.zipWithIndex.map{ case (cluster, i) =>
+        val xs = cluster.getVectors.asScala.map(_.get(0)).reduce(_ + _)
+        val ys = cluster.getVectors.asScala.map(_.get(1)).reduce(_ + _)
+        val count = cluster.getVectors.size().toDouble
+
+        s"Cluster's centroid ${i} (${xs/count}, ${ys/count})"
+      }.foreach(println)
+      algo.printStatistics()
+    }
+
+    val r  = params.epsilon()/2.0
+    val r2 = math.pow(r, 2)
+    def computeCenters(p1: ST_Point, p2: ST_Point): (Point, Point) = {
+      var h = geofactory.createPoint(new Coordinate(-1.0,-1.0))
+      var k = geofactory.createPoint(new Coordinate(-1.0,-1.0))
+      val X: Double = p1.x - p2.x
+      val Y: Double = p1.y - p2.y
+      val D2: Double = math.pow(X, 2) + math.pow(Y, 2)
+      if (D2 != 0.0){
+        val root: Double = math.sqrt(math.abs(4.0 * (r2 / D2) - 1.0))
+        val h1: Double = ((X + Y * root) / 2) + p2.x 
+        val k1: Double = ((Y - X * root) / 2) + p2.y 
+        val h2: Double = ((X - Y * root) / 2) + p2.x 
+        val k2: Double = ((Y + X * root) / 2) + p2.y 
+        h = geofactory.createPoint(new Coordinate(h1,k1))
+        k = geofactory.createPoint(new Coordinate(h2,k2))
+      }
+      (h, k)
+    }
+
+    clusters.map{ cluster =>
+      val points = cluster.getVectors().asScala.map{ v =>
+        ST_Point(v.getTid, v.get(0), v.get(1), v.getT)
+      }
+      val pairs = points.cross(points)
+        .filter( p => p._1 < p._2)
+        .map(p => (p, p._1.distance(p._2)))
+        .filter(p => p._2 <= params.epsilon())
+        .map{ p =>
+          val p1 = p._1._1
+          val p2 = p._1._2
+          computeCenters(p1, p2)
+        }.toList
+      val centers = pairs.map(_._1).union(pairs.map(_._2))
+      val rtree = new com.vividsolutions.jts.index.strtree.STRtree()
+      for(point <- points.map(_.getJTSPoint)){
+        rtree.insert(point.getEnvelopeInternal, point)
+      }
+      val disks = centers.map{ center =>
+        rtree.query(center.buffer(r).getEnvelopeInternal).asScala
+          .map(_.asInstanceOf[Point])
+          .filter(point => center.distance(point) <= r)
+          .map(point => (center, point)).groupBy(_._1).toList.map{ p =>
+            val key = p._1 // the center
+            val value = p._2.toList.map(_._2) // list of points
+            (key, value)
+          }
+        .filter(_._2.size > params.mu())
+      }
+
+      // Prune duplicates and redundant disks...
+    }
 
     timer = clocktime
     stage = "Session close"
@@ -151,6 +242,7 @@ class DBScanOnSparkConf(args: Seq[String]) extends ScallopConf(args) {
   val m:          ScallopOption[Int]     = opt[Int]     (default = Some(200))
   val minpts:     ScallopOption[Int]     = opt[Int]     (default = Some(5))
   val epsilon:    ScallopOption[Double]  = opt[Double]  (default = Some(5.0))
+  val mu:         ScallopOption[Int]     = opt[Int]     (default = Some(5))
 
   verify()
 }
