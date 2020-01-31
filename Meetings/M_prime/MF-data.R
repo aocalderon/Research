22b17c83c343c5ecd16aa3e37a40c2ea32f3ebda
aocalderon <andress.calderon@gmail.com>
2020-01-31 11:31:42 -0800

    Preparing next meeting...

---
+++ Meetings/M_prime/MF-data.R
@@ -0,0 +1,15 @@
+require(tidyverse)
+
+data = read_tsv("~/Documents/PhD/Research/Meetings/M_prime/MF-data.txt", col_names = F) %>%
+  rename(appId = X1, Phase = X2, Time = X3, Instant = X4, Finer = X5, Coarse = X6) %>% 
+  group_by(Phase, Coarse, Finer) %>% summarise(Time = mean(Time)) 
+
+p = ggplot(data = data, aes(x = factor(Finer), y = Time, fill = factor(Phase))) +
+  geom_bar(stat="identity", position=position_dodge(width = 0.75), width = 0.7) + 
+  facet_wrap(~Coarse, ncol = 1) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(x="Number of partitions in finer grid", y="Time [s]", title="Execution time for maximal disk finding",
+       subtitle="Per number of partition in coarse grid", fill="Phases") 
+plot(p)
+
+ggsave("~/Documents/PhD/Research/Meetings/M_prime/MF-data.pdf", width = 8, height = 7, device = "pdf")
