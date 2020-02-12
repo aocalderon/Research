bbcdebc10d789e7f04a2529d6d50560a987d29bc
Andres Calderon <acald013@ucr.edu>
2020-02-11 20:26:23 -0800

    Exploring spark settings...

--- Scripts/Scala/Geo/RDD/GeoTesterRDD.sh
+++ Scripts/Scala/Geo/RDD/GeoTesterRDD.sh
@@ -12,20 +12,22 @@ LISTENER=spark.extraListeners=TaskSparkListener
 LOG_FILE=/home/acald013/Spark/2.4/conf/log4j.properties
 
 MASTER=yarn
-EXECUTORS=18
-CORES=3
+EXECUTORS=12
+CORES=9
 DMEMORY=18g
-EMEMORY=10g
+EMEMORY=30g
 
-DATASET=/user/acald013/Datasets/LA/LA_25KTrajs/LA_25KTrajs_19.tsv
+DATASET=/user/acald013/Datasets/LA/LA_25KTrajs/LA_25KTrajs_1.tsv
 
-spark-submit \
-    --files $LOG_FILE \
+spark-submit --conf spark.driver.maxResultSize=10g \
     --jars ${SPARK_JARS}geospark-1.2.0.jar,${SPARK_JARS}geospark-sql_2.3-1.2.0.jar,${SPARK_JARS}scallop_2.11-3.1.5.jar,${SPARK_JARS}utils_2.11.jar \
-    --conf $LISTENER \
-    --conf spark.driver.extraJavaOptions=-Dlog4j.configuration=file:$LOG_FILE \
     --master $MASTER --deploy-mode client \
-    --num-executors $EXECUTORS --executor-cores $CORES --executor-memory $EMEMORY --driver-memory $DMEMORY \
+    --num-executors $EXECUTORS --executor-cores $CORES --executor-memory $EMEMORY \
     --class $CLASS_NAME $CLASS_JAR \
     --input $DATASET \
     --epsilon $EPSILON --partitions $PARTITIONS --gridtype $GRIDTYPE --indextype $INDEXTYPE
+
+#--driver-memory $DMEMORY
+#    --files $LOG_FILE \
+#    --conf $LISTENER \
+#    --conf spark.driver.extraJavaOptions=-Dlog4j.configuration=file:$LOG_FILE \
