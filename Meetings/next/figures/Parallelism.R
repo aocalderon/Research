4c9cbb901d458ca43c2b7eccfc46dc69d26c9cd6
aocalderon <andress.calderon@gmail.com>
2020-02-28 14:05:59 -0800

    Preparing next meeting...

--- Meetings/next/figures/Parallelism.R
+++ Meetings/next/figures/Parallelism.R
@@ -11,8 +11,7 @@ spark = log %>% filter(grepl(value, pattern = "SparkSubmit")) %>%
   separate(value, into = c(NA, "appId", "command"), sep = "\\|")
 spark$params = spark$command %>% map(getParams)
 spark = spark %>% separate(params, into = c(NA,"Epsilon", NA,"Partitions", NA,"Parallelism", NA,"Partitions2"), sep = " ") %>%
-  #filter(Parallelism == 324) %>%
-  select(appId, Epsilon, Partitions)
+  select(appId, Epsilon, Partitions, Parallelism) 
 
 fieldsGeoTester = c("Timestamp","Tag1","appId","Phase","Tag2","Time")
 mf = log %>% filter(grepl(value, pattern = "\\|Time\\|")) %>% 
@@ -20,22 +19,15 @@ mf = log %>% filter(grepl(value, pattern = "\\|Time\\|")) %>%
   mutate(Time = as.numeric(Time)) %>%
   select(appId, Phase, Time)
 
-data = mf %>% inner_join(spark, by = c("appId")) %>% select(Epsilon, Partitions, Phase, Time)
+data = mf %>% inner_join(spark, by = c("appId"))
 
-data1 = data %>% group_by(Phase, Partitions, Epsilon) %>% summarise(Time = mean(Time))
+data1 = data %>% group_by(Phase, Partitions, Parallelism, Epsilon) %>% summarise(Time = mean(Time))
+data2 = data1 %>% group_by(Partitions, Parallelism, Epsilon) %>% summarise(Time = sum(Time))
 
-p = ggplot(data = data1, aes(x = factor(Partitions), y = Time, fill = Epsilon)) +
-  geom_bar(stat="identity", position=position_dodge(width = 0.75), width = 0.7) + 
-  facet_wrap(~Phase, ncol = 1) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-  labs(x="Number of partitions", y="Time [s]", title="Execution time initial phases MF") 
-plot(p)
-ggsave("PartitionTimeEpsilonPhase.pdf", width = 8, height = 7, device = "pdf")
-
-data2 = data1 %>% group_by(Partitions, Epsilon) %>% summarise(Time = sum(Time))
 p = ggplot(data = data2, aes(x = factor(Partitions), y = Time, fill = Epsilon)) +
   geom_bar(stat="identity", position=position_dodge(width = 0.75), width = 0.7) +
+  facet_wrap(~Parallelism, ncol = 1) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-  labs(x="Number of partitions", y="Time [s]", title="Total execution time MF") 
+  labs(x="Number of partitions", y="Time [s]", title="Total execution time MF by parallelism") 
 plot(p)
-ggsave("PartitionTimeEpsilonPhaseTotal.pdf", width = 8, height = 7, device = "pdf")  
+ggsave("Parallelism.pdf", width = 8, height = 7, device = "pdf")  
