ae41ed003207613aebe66aa53fb3facf3c2139cb
acalderon <acald013@ucr.edu>
2023-05-26 19:41:26 -0700

    Preparing quadtree report...

--- Scripts/R/Cliques2/Tester.R
+++ Scripts/R/Cliques2/Tester.R
@@ -8,8 +8,7 @@ distance <- function(x1, y1, x2, y2){
   y2 = y * y
   return (sqrt(x2 + y2))
 }
-points = read_tsv("data/TT320_C500.tsv", col_names = c("x", "y", "cid", "oid", "tid")) |>
-  filter(cid == 94) |>
+points = read_tsv("/home/and/Research/Datasets/dense2.tsv", col_names = c("oid", "x", "y", "tid")) |>
   select(x, y, oid)
 
 p1 = points
@@ -19,10 +18,11 @@ names(p2) = c("x2", "y2", "oid2")
 pairs0 = crossing(p1, p2) 
 pairs1 = pairs0 |> filter(oid1 < oid2)
 pairs2 = pairs1 |> mutate(dist = distance(x1,y1,x2,y2))
-pairs3 = pairs2 |> filter(dist <= 5.001)
+pairs3 = pairs2 |> filter(dist <= 10.001)
 
 ggplot(points) + geom_point(aes(x=x, y=y)) +
   geom_point(data = pairs3, aes(x=x1, y=y1, colour = "blue")) +
   geom_point(data = pairs3, aes(x=x2, y=y2, colour = "red"))
 
-
+data = read_tsv("pairs.tsv", col_names = c("key", "coord", "s1", "s2", "pairs"))
+sum(data$pairs)
