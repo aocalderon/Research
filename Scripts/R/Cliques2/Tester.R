c35e54b5f2b1cd5ad89808bf76ecc9a072a54e92
Andres Calderon <andress.calderon@gmail.com>
2023-05-28 16:48:18 -0700

    Sync...

--- Scripts/R/Cliques2/Tester.R
+++ Scripts/R/Cliques2/Tester.R
@@ -18,7 +18,7 @@ names(p2) = c("x2", "y2", "oid2")
 pairs0 = crossing(p1, p2) 
 pairs1 = pairs0 |> filter(oid1 < oid2)
 pairs2 = pairs1 |> mutate(dist = distance(x1,y1,x2,y2))
-pairs3 = pairs2 |> filter(dist <= 10.001)
+pairs3 = pairs2 |> filter(dist <= 20.001)
 
 ggplot(points) + geom_point(aes(x=x, y=y)) +
   geom_point(data = pairs3, aes(x=x1, y=y1, colour = "blue")) +
