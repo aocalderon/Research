837483a05f901f13cdf68bf90c65ef51e404da46
Andres Calderon <acald013@ucr.edu>
2024-05-20 11:32:48 -0700

    Sync from server...

--- Scripts/Scala/PFlock/bash/mf.sh
+++ Scripts/Scala/PFlock/bash/mf.sh
@@ -3,16 +3,20 @@
 
 N=$1
 
-DATASET="PFlock/LA/LA_25K"
-ENDTIME=15
-CS=( 25 50 75 100 200 500 1000 2000 3000 ) # epsilon
+DATASET="PFlock/LA/LA_50K"
+ENDTIME=30
+ES=( 4 6 8 ) # epsilon
+CS=( 75 100 200 500 1000 2000 3000 ) # capacity
 
 for n in `seq 1 $N`
 do
+    for E in "${ES[@]}"
+    do
 	for C in "${CS[@]}"
 	do
-		echo "Running $n..."
-		echo "./tester_spark --dataset $DATASET --endtime $ENDTIME --method PSI --epsilon 15 --mu 3 --capacity $C --fraction 0.5 --master yarn"
-		./tester_spark --dataset $DATASET --endtime $ENDTIME --method PSI --epsilon 15 --mu 3 --capacity $C --fraction 0.5 --master yarn
+	    echo "Running $n..."
+	    echo "./tester_spark --dataset $DATASET --endtime $ENDTIME --method PSI --epsilon $E --mu 3 --capacity $C --fraction 0.5 --master yarn"
+	    ./tester_spark --dataset $DATASET --endtime $ENDTIME --method PSI --epsilon $E --mu 3 --capacity $C --fraction 0.5 --master yarn
 	done
+    done
 done
