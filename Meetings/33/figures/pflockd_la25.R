ab7a8fa5f3c765cae5d48c92bf1e001c87a28fd9
Andres Calderon <acald013@ucr.edu>
2024-05-24 09:01:42 -0700

    Sync from dblab...

---
+++ Meetings/33/figures/pflockd_la25.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+fields1 <- c("ts", "epoch", "host", "tag", "n1", "appId", "n2", "dataset", "epsilon", "mu", "delta", "method", "partitions", "sdist", "step", "stage", "time")
+pflock2 <- enframe(read_lines("pflock2_la25.txt"), value = "line") |>
+  filter(str_detect(line, 'TIME')) |>
+  separate(col = line, into = fields1, sep = "\\|") |>
+  mutate(time = as.numeric(time), epsilon = as.numeric(epsilon)) |>
+  select(epsilon, stage, time) |>
+  group_by(epsilon, stage) |> summarise(time = mean(time))
+
+p = ggplot(pflock2, aes(x = factor(epsilon), y = time, fill = stage)) + 
+  geom_col(width = 0.7, position="dodge") + 
+  labs(x="Epsilon (m)", y="Time (s)") 
+plot(p)
+
+W = 8
+H = 6
+ggsave(paste0("pflock2la25_time.pdf"), width = W, height = H)
+
+fields2 <- c("ts", "epoch", "host", "tag", "n1", "appId", "n2", "dataset", "epsilon", "mu", "delta", "method", "partitions", "sdist", "step", "stage", "n")
+pflock2_info <- enframe(read_lines("pflock2_la25.txt"), value = "line") |>
+  filter(str_detect(line, 'INFO')) |>
+  filter(str_detect(line, 'Safe') | str_detect(line, 'Partial') ) |>
+  separate(col = line, into = fields2, sep = "\\|") |>
+  mutate(n = as.numeric(n), epsilon = as.numeric(epsilon)) |>
+  select(epsilon, stage, n) |> distinct() |> filter(n != 10312) |> filter(n != 7276)
+p = ggplot(pflock2_info, aes(x = factor(epsilon), y = n, fill = stage)) + 
+  geom_col(width = 0.7, position="dodge") + 
+  labs(x="Epsilon (m)", y="Number of flocks") 
+plot(p)  
+ggsave(paste0("pflock2la25_info.pdf"), width = W, height = H)
+
+fields1 <- c("ts", "epoch", "host", "tag", "n1", "appId", "n2", "dataset", "epsilon", "mu", "delta", "method", "partitions", "sdist", "step", "stage", "time")
+pflock3 <- enframe(read_lines("pflock3_la25.txt"), value = "line") |>
+  filter(str_detect(line, 'TIME')) |>
+  separate(col = line, into = fields1, sep = "\\|") |>
+  mutate(time = as.numeric(time)) |>
+  select(epsilon, stage, step, time)
+
+pflock3_safe <- pflock3 |> filter(stage == "Safe") |> select(epsilon, stage, time) |>
+  group_by(epsilon, stage) |> summarise(time = mean(time))
+pflock3_step <- pflock3 |> filter(stage == "Partial") |> select(epsilon, step, time)
+p = ggplot(pflock3_step, aes(x = epsilon, y = time, fill = step)) + 
+  geom_col(width = 0.7, position="dodge") + 
+  labs(x="Epsilon (m)", y="Time (s)") 
+plot(p)  
+ggsave(paste0("pflock3la25_time.pdf"), width = W, height = H)
