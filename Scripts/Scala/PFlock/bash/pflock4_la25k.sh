21b7c30e23991b65a81324390be2bad21d196020
Andres Calderon <acald013@ucr.edu>
2024-06-03 15:52:52 -0700

    Sync from server...

---
+++ Scripts/Scala/PFlock/bash/pflock4_la25k.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+N=$1
+
+DATASET="PFlock/LA_25K"
+FRACTION=0.1
+ENDTIME=60
+CS=( 2300 3000 5000 ) 
+ES=( 30 ) # epsilon
+SD=( 15 ) # speed distance
+SS=( 8 ) # step
+
+for n in `seq 1 $N`
+do
+    for e in "${!ES[@]}"
+    do
+	for S in "${SS[@]}"
+	do
+	    for C in "${CS[@]}"
+	    do
+		echo "PFlock4 Running... $n"
+		echo "./pflock4_spark --dataset $DATASET --sdist ${SD[e]} --epsilon ${ES[e]} --mu 3 --delta 3 --method PSI --master yarn --capacity $C --fraction $FRACTION --endtime $ENDTIME"
+		./pflock4_spark --dataset $DATASET --sdist ${SD[e]} --epsilon ${ES[e]} --mu 3 --delta 3 --method PSI --master yarn --capacity $C --fraction $FRACTION --endtime $ENDTIME
+	    done
+	done
+    done
+done
