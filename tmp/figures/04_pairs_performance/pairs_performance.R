309b22142166d9cab67886c97ff81ed9f2df5e59
Andres Calderon <andress.calderon@gmail.com>
2024-10-03 14:49:50 -0700

    Sync from laptop...

--- tmp/figures/04_pairs_performance/pairs_performance.R
+++ tmp/figures/04_pairs_performance/pairs_performance.R
@@ -1,28 +1,6 @@
 library(tidyverse)
 library(latex2exp)
 
-# fields <- c("ts","start","host", "tag", "z", "appId","partitions","dataset","epsilon","mu","delta","method","stage","time")  
-# data0 <- enframe(read_lines( "pairs_performance.txt" ), value = "line") |>
-#   filter(str_detect(line, 'TIME')) |>
-#   separate(col = line, into = fields, sep = "\\|") |>
-#   mutate(stage = str_trim(stage), epsilon = as.numeric(epsilon), time = as.numeric(time)) |>
-#   filter(stage == "Total") |>
-#   filter(epsilon <= 20) |>
-#   select(dataset, method, epsilon, time) |> 
-#   group_by(dataset, method, epsilon) |> summarise(time = mean(time)) 
-# 
-# pairs <- enframe(read_lines( "pairs_performance.txt" ), value = "line") |>
-#   filter(str_detect(line, 'INFO')) |>
-#   filter(str_detect(line, 'BFE')) |>
-#   filter(str_detect(line, 'Pairs')) |>
-#   separate(col = line, into = fields, sep = "\\|") |>
-#   mutate(stage = str_trim(stage), epsilon = as.numeric(epsilon), pairs = as.numeric(time)) |>
-#   filter(epsilon <= 20) |>
-#   select(dataset, epsilon, pairs) |> 
-#   group_by(dataset, epsilon) |> summarise(pairs = max(pairs)) 
-#
-# data <- data0 |> inner_join(pairs, by = c("dataset", "epsilon"))
-
 data <- read_tsv("pairs_performance.tsv")
 
 p = ggplot(data, aes(x = pairs, y = time, shape = method, color = method)) +
@@ -31,7 +9,8 @@ p = ggplot(data, aes(x = pairs, y = time, shape = method, color = method)) +
   scale_color_discrete("Method") +
   scale_shape_discrete("Method") +
   facet_wrap(~ epsilon) +
-  theme_bw() 
+  theme_bw() +
+  theme(legend.position="bottom") 
 plot(p)  
 
 W = 8
