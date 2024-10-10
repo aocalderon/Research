0a8225f2093ff7578f07b49615d15bc33a686279
Andres Calderon <andress.calderon@gmail.com>
2024-10-10 15:01:43 -0700

    Sync...

--- tmp/figures/10_cmbc_variants/cmbc_bfe.R
+++ tmp/figures/10_cmbc_variants/cmbc_bfe.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(latex2exp)
 
-cmbc <- read_tsv("cmbc_bfe.tsv")
-bfe  <- read_tsv("bfe.tsv") |> filter(Stage == "Maximals") |> select(epsilon, time) |> mutate(Method = "BFE")
+cmbc <- read_tsv("cmbc_bfe.tsv") 
+bfe  <- read_tsv("bfe.tsv") |> filter(Stage == "Maximals") |> 
+  select(epsilon, time) |> mutate(Method = "BFE")
 p = ggplot(data = cmbc, aes(x = factor(epsilon), y = time, fill = Variant)) +
   geom_bar(stat="identity", position=position_dodge(width = 0.75), width = 0.7) + 
   geom_line(data = bfe, aes(group = Method, fill = NULL, color=Method)) +
@@ -14,4 +15,4 @@ plot(p)
 
 W = 8
 H = 6
-ggsave(paste0("cmbc_variants_bfe.pdf"), width = W, height = H)
+#ggsave(paste0("cmbc_variants_bfe.pdf"), width = W, height = H)
